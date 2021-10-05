dd48a6d9e0b4e74de91ada14e1d85de683e2d563
sumon6638-sm <mdsumoncse19@gmail.com>
2021-10-04 21:16:50 -0400

    check_readme

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,22 +3,22 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-dark'>
-            <div className="d-flex">
+            <div className="container d-flex">
                 <div className="col-md-6 text-warning">
                     <h3 className='text-start ms-2'>GET IN TOUCH</h3>
                     <div className="d-flex text-light">
-                        <div className="col-md-6 text-start ms-2">
+                        <div className="col-md-6 text-start ms-2 pe-2">
                             <p>7-14, Nikunja Housing Society South Khulshi, Chittagong</p>
                         </div>
 
-                        <div className="col-md-6 text-start ms-2">
+                        <div className="col-md-6 text-start ms-2 px-2">
                             <p>88 031-2869877 , 88 031-2869899</p>
                             <p>mdsumoncse19@gmail.com</p>
                         </div>
                     </div>
                 </div>
 
-                <div className="col-md-6 text-warning">
+                <div className="col-md-6 text-warning ms-5">
                     <h3 className='text-start ms-5'>USEFUL LINKS</h3>
                     <div className="text-light col-md-6 text-start ms-5">
                         <li>Bangladesh University</li>
