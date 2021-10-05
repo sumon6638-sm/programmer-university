f2cff43c595eb3ba9834021b1d43cab3f0eddf0c
sumon6638-sm <mdsumoncse19@gmail.com>
2021-10-04 23:56:22 -0400

    footer_icon2

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,17 +6,17 @@ const Contact = () => {
             <div className='container card my-5 shadow-lg py-5'>
                 <h1 className='fw-bold'>CONTACT</h1>
                 <div className='text-start ms-5'>
-                    <p><strong>Location: </strong>7-14, Nikunja Housing Society, South Khulshi, Chittagong.</p>
+                    <p><strong><i class="fas fa-location-arrow"></i> Location: </strong>7-14, Nikunja Housing Society, South Khulshi, Chittagong.</p>
 
-                    <p><strong>Permanent Campus: </strong>Plot# S-1, CDA Kolpolok Residential Area, Bakalia, Chittagong.</p>
+                    <p><strong><i class="fas fa-map-marked-alt"></i> Permanent Campus: </strong>Plot# S-1, CDA Kolpolok Residential Area, Bakalia, Chittagong.</p>
 
-                    <p><strong>Contact: </strong>88 031-2869877</p>
+                    <p><strong><i class="fas fa-id-card"></i> Contact: </strong>88 031-2869877</p>
 
-                    <p><strong>Mobile: </strong>01851120791, 01773225500, 01773225511</p>
+                    <p><strong><i class="fas fa-mobile-alt"></i> Mobile: </strong>01851120791, 01773225500, 01773225511</p>
 
-                    <p><strong>FAX: </strong>+880312869966</p>
+                    <p><strong><i class="fas fa-fax"></i> FAX: </strong>+880312869966</p>
 
-                    <p><strong>Email: </strong>admission@sumon.edu.bd</p>
+                    <p><strong><i class="far fa-paper-plane"></i> Email: </strong>admission@sumon.edu.bd</p>
                 </div>
             </div>
 
