441db36755d4a961628a7df9b3b3594d43bb1947
sumon6638-sm <mdsumoncse19@gmail.com>
2021-10-05 16:31:28 -0400

    navStyle

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -3,14 +3,20 @@ import { NavLink } from 'react-router-dom';
 
 
 const Navbar = () => {
+    const activeTab = {
+        fontWeight: 'bold',
+        border: '2px solid yellow',
+        borderRadius: '5px'
+    }
+
     return (
         <div className='header bg-dark'>
             <nav className='container navbar navbar-dark text-center'>
-                <NavLink className='ms-3 text-warning text-decoration-none' to='/home'>Home</NavLink>
-                <NavLink className='text-warning text-decoration-none' to='/courses'>Courses</NavLink>
-                <NavLink className='text-warning text-decoration-none' to='/instructor'>Instructor</NavLink>
-                <NavLink className='text-warning text-decoration-none' to='/about'>About</NavLink>
-                <NavLink className=' text-warning text-decoration-none' to='/contact'>Contact Us</NavLink>
+                <NavLink className='ms-3 text-warning text-decoration-none' activeStyle={activeTab} to='/home'>Home</NavLink>
+                <NavLink className='text-warning text-decoration-none' activeStyle={activeTab} to='/courses'>Courses</NavLink>
+                <NavLink className='text-warning text-decoration-none' activeStyle={activeTab} to='/instructor'>Instructor</NavLink>
+                <NavLink className='text-warning text-decoration-none' activeStyle={activeTab} to='/about'>About</NavLink>
+                <NavLink className=' text-warning text-decoration-none' activeStyle={activeTab} to='/contact'>Contact Us</NavLink>
                 <button className='btn btn-outline-warning'><i class="fas fa-sign-in-alt"></i> Login/Register</button>
             </nav>
         </div>
